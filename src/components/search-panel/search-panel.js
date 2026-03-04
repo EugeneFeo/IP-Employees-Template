eaee24ddf88476b80aae96b18221bb474c9fc84d
EugeneFeo <dossier1991@gmail.com>
2026-03-04 16:51:11 +0100

    IP Emloyees

---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,12 @@
+import "./search-panel.css";
+const SearchPanel = () => {
+  return (
+    <input
+      className="form-control search-input"
+      type="text"
+      placeholder="Найти сотрудника"
+    />
+  );
+};
+
+export default SearchPanel;
