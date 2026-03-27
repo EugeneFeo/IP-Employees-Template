65a831dd80a4139d8ad7397f345d2aca8a1029c5
EugeneFeo <dossier1991@gmail.com>
2026-03-27 19:43:37 +0100

    project employees-events and filters

--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -1,12 +1,29 @@
+import { Component } from "react";
 import "./search-panel.css";
-const SearchPanel = () => {
-  return (
-    <input
-      className="form-control search-input"
-      type="text"
-      placeholder="Найти сотрудника"
-    />
-  );
-};
+class SearchPanel extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: "",
+    };
+  }
+
+  onUpdateSearch = (e) => {
+    const term = e.target.value;
+    this.setState({ term });
+    this.props.onUpdateSearch(term);
+  };
+  render() {
+    return (
+      <input
+        className="form-control search-input"
+        type="text"
+        placeholder="Найти сотрудника"
+        value={this.state.term}
+        onChange={this.onUpdateSearch}
+      />
+    );
+  }
+}
 
 export default SearchPanel;
