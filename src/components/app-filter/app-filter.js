f9e1d9b8a5f2ad2290f27089b2c7945e557b6146
EugeneFeo <dossier1991@gmail.com>
2026-03-31 16:33:54 +0200

    project employees

--- src/components/app-filter/app-filter.js
+++ src/components/app-filter/app-filter.js
@@ -1,20 +1,22 @@
 import "./app-filter.css";
 const AppFilter = (props) => {
   const buttonsData = [
-    { name: "all", label: "Все сотрудники" },
-    { name: "rise", label: "На повышение" },
-    { name: "salary", label: "З/П больше 1000$" },
+    { name: "all", label: "Все сотрудники", colored: false },
+    { name: "rise", label: "На повышение", colored: false },
+    { name: "salary", label: "З/П больше 1000$", colored: true },
   ];
 
-  const buttons = buttonsData.map(({ name, label }) => {
+  const buttons = buttonsData.map(({ name, label, colored }) => {
     const active = props.filter === name;
     const clazz = active ? "btn-light" : "btn-outline-light";
+    const style = colored ? { color: "gold" } : null;
     return (
       <button
         key={name}
         className={`btn ${clazz}`}
         type="button"
         onClick={() => props.onFilterSelect(name)}
+        style={style}
       >
         {label}
       </button>
